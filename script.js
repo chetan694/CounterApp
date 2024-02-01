4693cdff2641c5ac7d0e3fa9ceceabac865d0625
chetan694 <109981808+chetan694@users.noreply.github.com>
2024-02-02 00:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const countValue=document.querySelector('#counter');
+const countvalue=document.querySelector('#counter');
 
-const increment = () =>{
-    let value=pasrseIntcountVlaue.innerText;
+function increment(){
+ let value = parseInt(countvalue.innerText);
 
+ value=value+1;
+
+ countvalue.innerText=value;
 }
 
-const decrement=() =>
-{
-
+function decrement(){
+    let value = parseInt(countvalue.innerText);
+   
+    value=value-1;
+   
+    countvalue.innerText=value;
 }
